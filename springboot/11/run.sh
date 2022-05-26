4a1e268dc82360c803354b2007fe4c48a2ac53d1
gi388 <m15629811052@163.com>
2022-05-27 00:58:24 +0800

    add GC for bootstart

--- springboot/11/run.sh
+++ springboot/11/run.sh
@@ -10,18 +10,24 @@ process_init_file() {
 
 #initialize manage environments
 : ${SPRINGBOOT_PORT:=8080}
-: ${SPRINGBOOT_MEMORY:=500m}
+: ${SPRINGBOOT_MEMORY:=512m}
 : ${SPRINGBOOT_JAR_NAME:=boot.jar}
 : ${SPRINGBOOT_HOME:=/workdir}
 : ${SPRINGBOOT_ACTIVE_PROFILE:-uat}
+: ${SPRINGBOOT_PRINT_GC:=no}
 : ${SPRINGBOOT_OPTS:-x}
 : ${SPRINGBOOT_TZDATE:-Asia/Shanghai}
 
-: ${JAVA_OPTS:=-Dfile.encoding=UTF8 -Dsun.jnu.encoding=UTF8 -Djava.security.egd=file:/dev/./urandom}
+: ${JAVA_OPTS:=-Dfile.encoding=UTF8 -Dsun.jnu.encoding=UTF8 -Djava.security.egd=file:/dev/urandom}
 : ${JAVA_OPTS_EXTRA:-}
 
 if [ "${SPRINGBOOT_ACTIVE_PROFILE:+x}" = "x" ];then
 	SPRINGBOOT_OPTS="${SPRINGBOOT_OPTS} --spring.profiles.active=${SPRINGBOOT_ACTIVE_PROFILE}"
+fi
+
+# must create /logs directory which java have write permission first
+if [ "${SPRINGBOOT_PRINT_GC}" == "yes" ];then
+     JAVA_OPTS="${JAVA_OPTS} -Xloggc:/logs/gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M"
 fi
 
 if [ "${SPRINGBOOT_TZDATE:+x}" = "x" ];then
