718530b2ddc77e54c2b57bf7a1a0db94f2441df3
gi388 <m15629811052@163.com>
2022-06-15 13:41:20 +0800

    auto setup server id for k8s environment

--- mysql/5.7/make-config.sh
+++ mysql/5.7/make-config.sh
@@ -5,7 +5,6 @@
 : ${SET_MYSQLD_FILE:=no}
 : ${SET_MYSQLD_MASTER:=no}
 : ${SET_MYSQLD_SLAVE:=no}
-: ${SET_MYSQLD_SLAVE_MASTER_NAME:=masterdb}
 : ${SET_MYSQLD_REPL_PASSWORD:=repl}
 
 #standalone
@@ -17,8 +16,8 @@
 : ${MYSQLD_USER:=mysql}
 : ${MYSQLD_LOG_BIN_TRUST_FUNCTION_CREATORS:=0}
 : ${MYSQLD_EXPIRE_LOGS_DAYS:=0}
+
 #cluster
-: ${MYSQLD_SERVER_ID:=0}
 : ${MYSQLD_INNODB_FLUSH_LOG_AT_TRX_COMMIT:=2}
 : ${MYSQLD_GTID_MODE:=ON}
 : ${MYSQLD_ENFORCE_GTID_CONSISTENCY:=ON}
@@ -67,66 +66,64 @@ if [ "${SET_MYSQLD_FILE}" == "no" -a "$1" == "mysqld" ];then
                 echo "\n you cant set both MASTER and SLAVE state"
                 exit 1
         elif [ "${SET_MYSQLD_MASTER}" == "yes" ];then
-                if [ ${MYSQLD_SERVER_ID} = 0 ];then
-                        echo ' "MYSQLD_SERVER_ID" should not be set to 0'
-                        exit 1
-                else
-                        test "${MYSQLD_LOG_BIN:-x}" == "x" && echo "log_bin = mysql-binlog" >> ${config_file}
-                        ##make sure scripts are executed at last
-                        if [ ! -d "${MYSQLD_DATADIR}/mysql" ]; then
-                            cat <<-EOSQL >/docker-entrypoint-initdb.d/create_user.sql
-                                INSTALL PLUGIN rpl_semi_sync_master SONAME 'semisync_master.so';
-                                CREATE USER 'repl'@'%' IDENTIFIED BY '${SET_MYSQLD_REPL_PASSWORD}';
-                                GRANT REPLICATION SLAVE,REPLICATION CLIENT ON *.* TO 'repl'@'%';
-							EOSQL
-                            cat <<-"EOSQL" >/docker-entrypoint-initdb.d/zz-enable-semi.sh
-                                #!/bin/bash
-                                config_file="/etc/mysql/my.cnf"
-                                test "${MYSQLD_RPL_SEMI_SYNC_MASTER_ENABLED:-x}" == "x" && echo "rpl_semi_sync_master_enabled = ON" >> ${config_file}
-							EOSQL
-                            chmod +x /docker-entrypoint-initdb.d/zz-enable-semi.sh
-                        else
-                            test "${MYSQLD_RPL_SEMI_SYNC_MASTER_ENABLED:-x}" == "x" && echo "rpl_semi_sync_master_enabled = ON" >> ${config_file}
-                            echo 'plugin-load="rpl_semi_sync_master=semisync_master.so"' >> ${config_file}
-                        fi
+                ## auto setup server id
+                test "${MYSQLD_SERVER_ID:-x}" == "x" && echo "server_id = 1" >> ${config_file}
+                test "${MYSQLD_LOG_BIN:-x}" == "x" && echo "log_bin = mysql-binlog" >> ${config_file}
+
+                ##make sure scripts are executed at last
+                if [ ! -d "${MYSQLD_DATADIR}/mysql" ]; then
+                    cat <<-EOSQL >/docker-entrypoint-initdb.d/create_user.sql
+                        CREATE USER 'repl'@'%' IDENTIFIED BY '${SET_MYSQLD_REPL_PASSWORD}';
+                        GRANT REPLICATION SLAVE,REPLICATION CLIENT ON *.* TO 'repl'@'%';
+					EOSQL
+                fi
+                if [ "${MYSQLD_RPL_SEMI_SYNC_MASTER_ENABLED:-x}" == "x" ];then
+                    echo "rpl_semi_sync_master_enabled = ON" >> ${config_file}
+                    echo 'plugin-load="rpl_semi_sync_master=semisync_master.so"' >> ${config_file}
                 fi
                 echo "Starting Master Server...."
         elif [ "${SET_MYSQLD_SLAVE}" == "yes" ];then
-                if [ ${MYSQLD_SERVER_ID} = 0 ];then
-                        echo '"MYSQLD_SERVER_ID" should not be set to 0'
-                        exit 1
-                else
-                        test "${MYSQLD_RELAY_LOG:-x}" == "x" && echo "relay_log = mysql-relaylog" >> ${config_file}
-                        test "${MYSQLD_READ_ONLY:-x}" == "x" && echo "read_only = 1" >> ${config_file}
-                        ##make sure scripts are executed at last
-                        if [ ! -d "${MYSQLD_DATADIR}/mysql" ]; then
-                            cat <<-EOSQL >/docker-entrypoint-initdb.d/create_user.sql
-                                INSTALL PLUGIN rpl_semi_sync_slave SONAME 'semisync_slave.so';
-                                CHANGE MASTER TO MASTER_HOST='${SET_MYSQLD_SLAVE_MASTER_NAME}',
-                                MASTER_USER='repl',
-                                MASTER_PASSWORD='${SET_MYSQLD_REPL_PASSWORD}',
-                                MASTER_AUTO_POSITION=1 for CHANNEL '${SET_MYSQLD_SLAVE_MASTER_NAME}';
-                                start slave for channel '${SET_MYSQLD_SLAVE_MASTER_NAME}';
-							EOSQL
-                            cat <<-"EOSQL" >/docker-entrypoint-initdb.d/zz-enable-semi.sh
-                                #!/bin/bash
-                                config_file="/etc/mysql/my.cnf"
-                                test "${MYSQLD_RPL_SEMI_SYNC_SLAVE_ENABLED:-x}" == "x" && echo "rpl_semi_sync_slave_enabled = ON" >> ${config_file}
-							EOSQL
-                            chmod +x /docker-entrypoint-initdb.d/zz-enable-semi.sh
-                            #if slave set pull master to setup database ,you should specify this env
-                            if [ "${SET_MYSQLD_PULL_PASSWORD:-x}" != "x" ];then
-                                cat <<-"EOSQL" >/docker-entrypoint-initdb.d/aa-pull-master.sh
-                                    #!/bin/bash
-                                    get_databases=$(mysql -uroot -p${SET_MYSQLD_PULL_PASSWORD} --host=${SET_MYSQLD_SLAVE_MASTER_NAME}  -e "show databases;"  |grep -E -v "Database|information_schema|mysql|performance_schema|sys" )
-                                    mysqldump --single-transaction -uroot -p${SET_MYSQLD_PULL_PASSWORD} --default-character-set=utf8 --routines --events --triggers --master-data=2 --host=${SET_MYSQLD_SLAVE_MASTER_NAME} --databases ${get_databases} | "${mysql[@]}"
-								EOSQL
-                                chmod +x /docker-entrypoint-initdb.d/aa-pull-master.sh
-                            fi
-                        else
-                            test "${MYSQLD_RPL_SEMI_SYNC_SLAVE_ENABLED:-x}" == "x" && echo "rpl_semi_sync_slave_enabled = ON" >> ${config_file}
-                            echo 'plugin-load="rpl_semi_sync_slave=semisync_slave.so"' >> ${config_file}
-                        fi
+                if [ "${SET_MYSQLD_SLAVE_MASTER_NAME:-x}" == "x" ];then
+                    echo "Error Please setup SET_MYSQLD_SLAVE_MASTER_NAME for connecting to master"
+                    exit 1
+                fi
+
+                ## auto setup server id
+                if [ "${MYSQLD_SERVER_ID:-x}" == "x" ];then
+                        pod_index=($(echo $(hostname) | tr "-" "\n"))
+                        test ${#pod_index[@]} -lt 2 && echo "Not K8s environment, please setup MYSQLD_SERVER_ID" && exit 1
+                        expr 20 "+" ${pod_index[-1]} &>/dev/null
+                        test $? -ne 0 && echo "Not K8s environment, please setup MYSQLD_SERVER_ID" && exit 1
+                        index=$(expr 20 "+" ${pod_index[-1]})
+                        echo "server_id = ${index}" >> ${config_file}
+                fi
+
+                test "${MYSQLD_RELAY_LOG:-x}" == "x" && echo "relay_log = mysql-relaylog" >> ${config_file}
+                test "${MYSQLD_READ_ONLY:-x}" == "x" && echo "read_only = 1" >> ${config_file}
+
+                ##make sure scripts are executed at last
+                if [ ! -d "${MYSQLD_DATADIR}/mysql" ]; then
+                    cat <<-EOSQL >/docker-entrypoint-initdb.d/create_user.sql
+                        CHANGE MASTER TO MASTER_HOST='${SET_MYSQLD_SLAVE_MASTER_NAME}',
+                        MASTER_USER='repl',
+                        MASTER_PASSWORD='${SET_MYSQLD_REPL_PASSWORD}',
+                        MASTER_AUTO_POSITION=1 for CHANNEL '${SET_MYSQLD_SLAVE_MASTER_NAME}';
+                        start slave for channel '${SET_MYSQLD_SLAVE_MASTER_NAME}';
+					EOSQL
+                    #if slave set pull master to setup database ,you should specify this env
+                    if [ "${SET_MYSQLD_PULL_PASSWORD:-x}" != "x" ];then
+                        cat <<-"EOSQL" >/docker-entrypoint-initdb.d/aa-pull-master.sh
+                            #!/bin/bash
+                            get_databases=$(mysql -uroot -p${SET_MYSQLD_PULL_PASSWORD} --host=${SET_MYSQLD_SLAVE_MASTER_NAME}  -e "show databases;"  |grep -E -v "Database|information_schema|mysql|performance_schema|sys" )
+                            mysqldump --single-transaction -uroot -p${SET_MYSQLD_PULL_PASSWORD} --default-character-set=utf8 --routines --events --triggers --master-data=2 --host=${SET_MYSQLD_SLAVE_MASTER_NAME} --databases ${get_databases} | "${mysql[@]}"
+						EOSQL
+                        chmod +x /docker-entrypoint-initdb.d/aa-pull-master.sh
+                    fi
+                fi
+                ## open semisync by default
+                if [ "${MYSQLD_RPL_SEMI_SYNC_SLAVE_ENABLED:-x}" == "x" ];then
+                    echo "rpl_semi_sync_slave_enabled = ON" >> ${config_file}
+                    echo 'plugin-load="rpl_semi_sync_slave=semisync_slave.so"' >> ${config_file}
                 fi
                 echo "Starting Slave Server...."
         else
