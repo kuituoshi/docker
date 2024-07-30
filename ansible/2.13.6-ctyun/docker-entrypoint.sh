c11c1690538848e4c2ee2522957fad34d7232e2f
chenzhipeng <changel@changel.cn>
2024-07-30 14:48:45 +0800

    适配ansible双架构

--- ansible/2.13.6-ctyun/docker-entrypoint.sh
+++ ansible/2.13.6-ctyun/docker-entrypoint.sh
@@ -5,7 +5,5 @@ set -e
 
 # startup ssh-agent
 eval "$(ssh-agent -s)" >/dev/null
-chmod 400 /root/.ssh/id_rsa
-ssh-add /root/.ssh/id_rsa >/dev/null 2>&1
 echo "Welcome to use HNKZ tools for CTYun Integration"
 exec "$@"
---
+++ ansible/2.13.6/README.md
@@ -0,0 +1,6 @@
+### Build Image
+
+```shell
+image=fastcn-registry-changsha42.crs.ctyun.cn/hnkz/ansible:2.13.6
+docker buildx build -t $image --platform=linux/arm64,linux/amd64 . --push
+```
